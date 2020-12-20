71a315059b1b3d2af2a4433b3d4e919f6f07d77c
raedaltaki <raed.simon@gmail.com>
2020-12-20 08:07:36 -0500

    Handle Form Submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+//var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function()
+var createTaskHandler = function(event)
 {
+    event.preventDefault();
     var taskItemE1 = document.createElement("li");
     taskItemE1.textContent = "This is a new task.";
     taskItemE1.className = "task-item";
     tasksToDoEl.appendChild(taskItemE1);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+//buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
